f09eab35e026ea7f3429aca34231ca091ae28008
hieu <hieunm008@gmail.com>
2020-02-18 00:20:32 +0700

    first commit

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TimKiemViecLam
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "TinViecLam",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "TinViecLam", action = "DetailIndex", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TimKiemViecLam.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            var dao = new HomeDAO();
+            var model = dao.ListAs();
+            return View(model);
+        }
+       public ActionResult HomeMember()
+        {
+            return View();
+        }
+    }
+}
